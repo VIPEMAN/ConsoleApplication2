7a33a9c0f84af7b1d1928144a42e4921fc49b8e0
VIPEMAN <kirilin200375@gmail.com>
2024-06-14 12:36:25 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,15 @@
+﻿// ConsoleApplication2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+
+int main()
+{
+  cout << "Hello World!fuiwwwwthqeriutureithweruithwertuyuidgffffhrrreeeeeeeeeesdksdkjlsdfkjlrio\n";
+   
+
+}
